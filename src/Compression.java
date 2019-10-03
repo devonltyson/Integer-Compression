bdf550a879cc6b51d9954d3c29643a909eea0e07
Devon Tyson <devonltyson@txstate.edu>
2019-10-03 00:18:31 -0500

    better compression ratio

--- src/Compression.java
+++ src/Compression.java
@@ -7,76 +7,58 @@ import java.util.Scanner;
 
 public class Compression {
 	
+	public static int sum = 0;
+	public static float numBytes = 0;
+	public static byte sigBytes;
+	public static OutputStream outFile;
+	public static int fileNum = 1;
+	public static int count = 0;
+	public static byte indicator = 0;
+	
+	public static String fileIn = "Data_GPDF_0.01"; // file used for testing
+	public static String dat = ".dat";
+	public static String b = "_b";
+	
 	public static void main(String[] args) {
-
-		int fileNum = 1;
-		int numBytes = 0;
-		int sum = 0;
-		int count = 0;
-		byte sigBytes = 0;
-		String fileIn = "Data_GPDF_0.01"; // file used for testing
-		String dat = ".dat";
-		String b = "_b";
 		
 		try {
 			
 			File inFile = new File(fileIn + ".txt");
 			Scanner scan = new Scanner(inFile);
-			OutputStream outFile = new FileOutputStream(fileIn + fileNum + b + dat);
+			outFile = new FileOutputStream(fileIn + fileNum + b + dat);
 			
 			int num = 0;
 			while(scan.hasNextInt()) { // !eof
 				
-				num = scan.nextInt();
-				numBytes+=4;
-				count++;
-				
-				if(num < 256) {
-					sum += 10;
-					sigBytes = 1;
+				// write 4 numbers at a time
+				for(int i=0; i < 4; i++) {
 					
-				} else if(num >= 256 && num <= (Math.pow(2, 16)-1)) {
-					sigBytes = 2;
-					sum += 18;
-					
-				} else if(num >= (Math.pow(2, 16)) && num <= (Math.pow(2, 24)-1) ) {
-					sigBytes = 3;
-					sum += 26;
-					
-				} else {
-					sigBytes = 4;
-					sum += 34;
+					if(scan.hasNext()) {
+						num = scan.nextInt();
+						writeNum(num); 
+						
+						// adjust the indicator for the 4 numbers depending on index
+						if(i==0) {
+							indicator = (byte) (indicator | (sigBytes));
+						} else if(i==1) {
+							indicator = (byte) (indicator | (sigBytes << 2));
+						} else if(i==2) {
+							indicator = (byte) (indicator | (sigBytes << 4));
+						} else if(i==3) {
+							indicator = (byte) (indicator | (sigBytes << 6));
+						}
+						
+						//testing
+						//System.out.println("ind: " + indicator);
+					}
 				}
-				
-				if(numBytes >= 65536) { // 64k bytes per file
-					fileNum++;
-					outFile = new FileOutputStream(fileIn + fileNum + b);			
-					numBytes = 0;
-				}
-				
-				byte fourth = (byte) (num & 0x000000ff);
-				byte third = (byte) ((num & 0x0000ff00) >> 8);
-				byte second = (byte) ((num & 0x00ff0000) >> 16);
-				byte first = (byte) ((num & 0xff000000) >> 24);
-				
 				outFile.write(sigBytes); // write the indicator byte
-				
-				if(first > 0)
-					outFile.write(first);
-				
-				if(second > 0)
-					outFile.write(second);
-				
-				if(third > 0)
-					outFile.write(third);
-				
-				if(fourth > 0)
-					outFile.write(fourth);
+			//	numBytes += 1;
+				indicator = 0;
 			}
-			
+				
 			double compression_ratio = (count*32)/sum; // where count = number of 32 bit ints in a file
-								   // and sum = total bits after compression
-			
+													   // and sum = total bits after compression
 			System.out.println("Compression ratio: " + compression_ratio);
 			outFile.close();
 			scan.close();
@@ -85,5 +67,57 @@ public class Compression {
 			
 			e.printStackTrace();
 		}
+	}
+	
+	public static void writeNum(int num) throws IOException {
+		count++;
+				
+		if(num < 256) {
+			sum += 10;
+			sigBytes = 1;
+			numBytes+=(10/8);
+					
+		} else if(num >= 256 && num <= (Math.pow(2, 16)-1)) {
+			sigBytes = 2;
+			sum += 18;
+			numBytes+=(18/8);
+					
+		} else if(num >= (Math.pow(2, 16)) && num <= (Math.pow(2, 24)-1) ) {
+			sigBytes = 3;
+			sum += 26;
+			numBytes+=(26/8);
+					
+		} else {
+			sigBytes = 4;
+			sum += 34;
+			numBytes+=(34/8);
+		}
+				
+		if(numBytes >= 65536) { // 64k bytes per file
+			fileNum++;
+			outFile = new FileOutputStream(fileIn + fileNum + b + dat);			
+			numBytes = 0;
+		}
+				
+		byte fourth = (byte) (num & 0x000000ff);
+		byte third = (byte) ((num & 0x0000ff00) >> 8);
+		byte second = (byte) ((num & 0x00ff0000) >> 16);
+		byte first = (byte) ((num & 0xff000000) >> 24);
+				
+				
+		if(first > 0)
+			outFile.write(first);
+				
+		if(second > 0)
+			outFile.write(second);
+				
+		if(third > 0)
+			outFile.write(third);
+				
+		if(fourth > 0)
+			outFile.write(fourth);
+		
+		//testing
+		//System.out.println("sig: " + sigBytes);
 	}
 }
